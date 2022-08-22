379b31c57c36b8522d8a76f6d548a1b36c3f1e19
hyperL123 <jiabinhuang077@gmail.com>
2022-08-21 23:16:50 -0600

    1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 // tailwind.config.js
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  content: [
+    // Example content paths...
+    "./public/**/*.html",
+    "./src/**/*.{js,jsx,ts,tsx,vue}",
+  ],
   darkMode: "class",
   theme: {
     extend: {
