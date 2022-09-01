845fefde08cbf351aafdb3301c95b69e3f303b8d
hyperL123 <jiabinhuang077@gmail.com>
2022-09-01 00:02:28 -0600

    screen change

--- src/screens/SignUp/index.js
+++ src/screens/SignUp/index.js
@@ -4,9 +4,11 @@ import Box from "./componments/Box";
 
 const SignUp = () => {
   return (
-    <div className="flex h-[60rem] w-full  items-center justify-center bg-gray-100">
-      <PageTitle title="Sign Up" />
-      <Box />
+    <div className=" bg-gray-100">
+      <div className="flex h-screen items-center justify-center">
+        <PageTitle title="Sign Up" />
+        <Box />
+      </div>
     </div>
   );
 };
