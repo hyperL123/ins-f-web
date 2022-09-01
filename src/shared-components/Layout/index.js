d21000fed87f550e67c5b0d0855222758215d4ed
hyperL123 <jiabinhuang077@gmail.com>
2022-08-31 23:10:19 -0600

    h-screen

--- src/shared-components/Layout/index.js
+++ src/shared-components/Layout/index.js
@@ -7,7 +7,7 @@ import { useReactiveVar } from "@apollo/client";
 const Layout = ({ children }) => {
   let show = useReactiveVar(showVar);
   return (
-    <div className="flex h-full w-full flex-col items-center bg-[#FAFAFA] ">
+    <div className="flex w-full flex-col items-center bg-[#FAFAFA]">
       <Header />
       {show && (
         <div className="m-4 w-full items-center bg-black  py-1 pl-6  font-semibold text-white">
